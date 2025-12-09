a6e865d59f75eff2c7435fbe9669a187cd05c14f
cccaternberg <acaternberg@cloudbees.com>
2025-12-09 11:18:04 +0100

    add

--- Jenkinsfile-source.groovy
+++ Jenkinsfile-source.groovy
@@ -13,7 +13,7 @@ pipeline {
                 echo 'Hello World'
                 //triggerRemoteJob mode: trackProgressAwaitResult(allowAbort: false, scheduledTimeout: [timeoutStr: ''], startedTimeout: [timeoutStr: ''], timeout: [timeoutStr: '1d'], whenFailure: stopAsFailure(), whenScheduledTimeout: continueAsIs(), whenStartedTimeout: continueAsIs(), whenTimeout: continueAsFailure(), whenUnstable: continueAsUnstable()), remotePathMissing: continueAsIs(), remotePathUrl: 'jenkins://b52f0c5313985cbde80b7602b63bcc51/test-triggers/child'
                 //triggerRemoteJob parameterFactories: [[$class: 'SimpleString', name: 'paramKey1', value: 'paramtValueFromparent']], remotePathMissing: stopAsFailure(), remotePathUrl: "jenkins://${instanceID}/test-triggers/child"
-                triggerRemoteJob remotePathMissing: stopAsFailure(), remotePathUrl: getTargetInstanceID("//target","child")                //build 'child'
+                triggerRemoteJob remotePathMissing: stopAsFailure(), remotePathUrl: getTargetInstanceID("cjp://target","child")                //build 'child'
             }
         }
     }
