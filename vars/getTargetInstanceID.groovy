aac19b4bdf5de212ee3bf75d3d6604273c0772a6
cccaternberg <acaternberg@cloudbees.com>
2025-12-09 11:14:37 +0100

    add

--- vars/getTargetInstanceID.groovy
+++ vars/getTargetInstanceID.groovy
@@ -1,17 +1,17 @@
 import com.cloudbees.opscenter.context.remote.RemoteDirectory
 import com.cloudbees.opscenter.context.remote.RemotePath
 
-def call(String controllerNameAndJobPath){
+def call(String controllerName,String jobPath){
 
     /* The controller name or path to resolve.  "target"= controller_name, "child" = job_path/job_name
      Note: Does not work if you put the controller name only; the full path to the target job is required. `//` is important as prefix
     */
-    String controllerPath = "${controllerNameAndJobPath}"
-    RemotePath resolvedPath = RemoteDirectory.walk(controllerNameAndJobPath)
+    String controllerPath = "${controllerName}/${jobPath}"
+    RemotePath resolvedPath = RemoteDirectory.walk(controllerPath)
     if (resolvedPath != null) {
-        println "Instance ID for '${controllerNameAndJobPath}': ${resolvedPath.getInstanceId()}"
+        println "Instance ID for '${controllerPath}': ${resolvedPath.getInstanceId()}"
     } else {
-        println "Could not resolve path: ${controllerNameAndJobPath}"
+        println "Could not resolve path: ${controllerPath}"
     }
     return "jenkins://${resolvedPath.getInstanceId()}/${jobPath}"
 }
