e29270d0010a64e87ec2bc672b118634fdf8724c
cccaternberg <acaternberg@cloudbees.com>
2025-12-12 12:37:56 +0100

    add

--- Jenkinsfile-source.groovy
+++ Jenkinsfile-source.groovy
@@ -1,9 +1,7 @@
-//env.SHARED_LIB_TAG should be injected from (Folder) environment properties
 env.SHARED_LIB_TAG="main"
 library identifier: "ci-shared-library@${env.SHARED_LIB_TAG}", retriever: modernSCM(
         [$class: 'GitSCMSource',
          remote: 'https://github.com/cb-ci-templates/ci-poc-triggerRemoteJob.git'])
-
 
 pipeline {
     agent none
@@ -11,6 +9,7 @@ pipeline {
         stage('Hello') {
             steps {
                 echo 'Hello World'
+                triggerRemoteJob parameterFactories: [[$class: 'SimpleString', name: 'paramKey1', value: 'paramtValueFromparent']], remotePathMissing: stopAsFailure(), remotePathUrl: getTargetInstanceID("target","child")
                 triggerRemoteJob remotePathMissing: stopAsFailure(), remotePathUrl: getTargetInstanceID("target","child")                //build 'child'
             }
         }
